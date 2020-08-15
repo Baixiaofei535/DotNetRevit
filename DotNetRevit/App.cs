6598a4280f5e842ea5ab99c0637985f5d8364d3f
Coolicky <piotr.kulicki@gmail.com>
2020-08-15 20:47:05 +0100

    Add project files.

---
+++ DotNetRevit/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+
+namespace DotNetRevit
+{
+    public class App : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            AddRibbonPanel(application);
+            return Result.Succeeded;
+        }
+
+        private static void AddRibbonPanel(UIControlledApplication application)
+        {
+            string tab_name = "DotNetRevit";
+            application.CreateRibbonTab(tab_name);
+        }
+    }
+}
---
+++ DotNetRevit/Scripts/MainTab/Project/ProjectTools2_Stack/GetCentralPath/GetCentralPath.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetRevit.Scripts.MainTab.Project.ProjectTools2_Stack
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    internal class GetCentralPath : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            var modelPath = doc.GetWorksharingCentralModelPath();
+
+            var centralPath = ModelPathUtils.ConvertModelPathToUserVisiblePath(modelPath);
+
+            if (System.Windows.Forms.Control.ModifierKeys == Keys.Shift)
+            {
+                if (File.Exists(centralPath))
+                {
+                    Process.Start("explorer.exe", Path.GetDirectoryName(centralPath));
+                }
+            }
+            else
+            {
+                if (!doc.IsWorkshared)
+                {
+                    TaskDialog.Show("Get Central Path", "Model is Not Workshared");
+                    return Result.Cancelled;
+                }
+
+                TaskDialog.Show("Get Central Path", centralPath);
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
